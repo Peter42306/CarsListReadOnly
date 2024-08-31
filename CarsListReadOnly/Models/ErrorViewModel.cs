a80eeef2f6d06467f1708303e92b5b08c8e152a0
Petr Zalizko <pzalizko@gmail.com>
2024-08-31 12:44:41 +0300

    Add project files.

---
+++ CarsListReadOnly/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CarsListReadOnly.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
